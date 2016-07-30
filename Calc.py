394d9383ec6324fe8ad82e5367b1ae364b4bfc21
piotro <piotro@localhost>
2016-07-30 21:38:32 +0200

    Rework of classes, introduced Square expression. Using array and dictionary data structures

--- Calc.py
+++ Calc.py
@@ -1,59 +1,73 @@
 #Calculator by RealStudios
 #www.realstudiosen.tumblr.com
 #Written in Python 3.5.1
-#Adds num1, num2
 import math
 
 class Expression(object):
     def eval(self):
         raise NotImplementedError("Please implement this method")
 
-class Constant(Expression):
-    def __init__(self, value):
-        self.value = value
-    
-    def eval(self):
-        return self.value
-    
-class AddExpression(Expression):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    def print(self):
+        print(self.eval());
 
+    def readFloat(self, message):
+        return float(input(message))
+
+class OneArgExpression(Expression):
+    def readArguments(self):
+        self.arg = self.readFloat("Input the number: ")
+    
+
+class SquareExpression(OneArgExpression):
+    def eval(self):
+        return self.arg * self.arg
+
+    def op(self):
+        return "s"
+    
+
+class TwoArgExpression(Expression):
+    left = 0.0
+    right = 0.0
+
+    def readArguments(self):
+        self.left = self.readFloat("Input the first number: ")
+        self.right = self.readFloat("Input the second number: ")
+    
+class AddExpression(TwoArgExpression):
     def eval(self):
         return self.left + self.right
 
-class SubstractExpression(Expression):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    def op(self):
+        return "+"
 
+class SubstractExpression(TwoArgExpression):
     def eval(self):
         return self.left - self.right
 
-class MultiplyExpression(Expression):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    def op(self):
+        return "-"
 
+class MultiplyExpression(TwoArgExpression):
     def eval(self):
         return self.left * self.right
 
-class DivideExpression(Expression):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    def op(self):
+        return "*"
 
+class DivideExpression(TwoArgExpression):
     def eval(self):
         return self.left / self.right
 
-class PowerExpression(Expression):
-    def __init__(self, left, right):
-        self.left = left
-        self.right = right
+    def op(self):
+        return "/"
 
+class PowerExpression(TwoArgExpression):
     def eval(self):
         return math.pow(self.left, self.right)
+
+    def op(self):
+        return "^"
 
 
 #Function that allows to re-use the calculator and view the result   
@@ -64,40 +78,25 @@ def end():
 
 #Function that does all the job   
 def main():
-    operation = input("What do you want to do?(+, -, x, /, ^): ")
+    expressionArray = [ AddExpression(), SubstractExpression(), MultiplyExpression(), DivideExpression(), PowerExpression(), SquareExpression() ]
+
+    expressionMap = dict()
+    operators = ''
+    for e in expressionArray:
+        operators = operators + e.op() + ', '
+        expressionMap.update({ e.op(): e })
+
+    operation = input('What do you want to do?(' + operators + '): ')
     #Checks for a valid operation
-    if(operation != "+" and operation != "-" and operation != "x" and operation != "/" and operation != "^"):
-        print("You must enter a valid operation.")
-        main()
-    else:
-        var1 = float(input("Input the first number: "))
-        var2 = float(input("Input the second number: "))
-        if(operation == '+'):
-            expr = AddExpression(var1, var2)
-            print(expr.eval())
-            end()
-        elif(operation == '-'):
-            expr = SubstractExpression(var1, var2)
-            print(expr.eval())
-            end()
-        elif(operation =='x'):
-            expr = MultiplyExpression(var1, var2)
-            print(expr.eval())
-            end()
-        elif(operation =='^'):
-            expr = PowerExpression(var1, var2)
-            print(expr.eval())
-            end()
-        else:
-            if(var2 == 0.0):
-                print('Hey! What the heck you think you are doing!? Dividing by 0?')
-            else: 
-                expr = DivideExpression(var1, var2)
-                print(expr.eval())
-            end()
+    if (operation in expressionMap):
+        chosenExpression = expressionMap[operation]
+        chosenExpression.readArguments()
+        chosenExpression.print()
+
+    end()
 
 creator = 'Made by Real and piotro from RealStudios'
-ver = 'Version: Open Alpha 0.2.1'
+ver = 'Version: Open Alpha 0.3.0'
 print(creator)
 print(ver)
 main()
